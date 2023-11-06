4a55ba686cb5bfafbecb3ac2efe68ede375e3b6c
wonowonow <wonow_@naver.com>
2023-11-06 17:34:15 +0900

    feat: 게시글 수정, 삭제 비밀번호 미일치 시 예외 메세지 반환

--- src/main/java/com/sparta/spartaposts/controller/PostController.java
+++ src/main/java/com/sparta/spartaposts/controller/PostController.java
@@ -3,12 +3,21 @@ package com.sparta.spartaposts.controller;
 import com.sparta.spartaposts.dto.PostRequestDto;
 import com.sparta.spartaposts.dto.PostResponseDto;
 import com.sparta.spartaposts.service.PostService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 public class PostController {
+    private HttpHeaders makeUTF8Header(){
+        HttpHeaders returnResHeaders = new HttpHeaders();
+        returnResHeaders.add("Content-Type", "application/json;charset=UTF-8");
+
+        return returnResHeaders;
+    }
 
     @PostMapping("/posts")
     public PostResponseDto creatPost(@RequestBody PostRequestDto postRequestDto) {
@@ -29,15 +38,26 @@ public class PostController {
     }
 
     @PutMapping("/posts/{id}")
-    public Long updatePost(@PathVariable Long id, @RequestBody PostRequestDto postRequestDto) {
+    public ResponseEntity<String> updatePost(@PathVariable Long id, @RequestBody PostRequestDto postRequestDto) {
         PostService postService = new PostService();
-        return postService.updatePost(id, postRequestDto);
+        try {
+            postService.updatePost(id, postRequestDto);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(e.getMessage(), makeUTF8Header(), HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(id + "번이 정상적으로 수정 되었습니다.", makeUTF8Header(), HttpStatus.OK);
     }
 
     @DeleteMapping("/posts/{id}")
-    public Long deletePost(@PathVariable Long id, @RequestBody PostRequestDto postRequestDto) {
+    public ResponseEntity<String> deletePost(@PathVariable Long id, @RequestBody PostRequestDto postRequestDto) {
         PostService postService = new PostService();
-        return postService.deletePost(id, postRequestDto);
+        try {
+            postService.deletePost(id, postRequestDto);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(e.getMessage(), makeUTF8Header(), HttpStatus.BAD_REQUEST);
+            // 로그 변환해서 e.getMessage를 스트링으로 변환해야함
+        }
+        return new ResponseEntity<>(id + "번이 정상적으로 삭제 되었습니다", makeUTF8Header(), HttpStatus.OK);
+        // 리스폰스 엔티티
     }
-
 }
